5db6c7e84ef7d7ab6740ad52b1eba2353fe907fd
Aslan Aslan <aslanaksir@gmail.com>
2024-01-12 07:55:00 +0300

    Task2-4:Lesson 2

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// int n = 10;
+int[] arr = {2, 5, 4, 7, 8, 35, 1, 3, 8, 5};
+int i = 0;
+// while(i < n)
+while(i < arr.Length)
+{
+    // arr[i] = i+1;
+    if(arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    // Console.Write(arr(i));
+    // Console.Write(' ');
+    // Console.Write($"{arr[i]} ");
+    i = i+1;
+}
